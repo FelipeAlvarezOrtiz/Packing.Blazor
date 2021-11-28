90e776b2fda4e0204e80ce1b9547b6e605f0e226
Felipe Alvarez Ortiz <me@felipealvarez.dev>
2021-11-27 22:21:04 -0300

    - Se crea una respuesta global - Se agregan reglas de negocio de reportes - Se crea los logs

---
+++ Packing/Packing.Core/Log/LogSoftware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Packing.Core.Log
+{
+    public class LogSoftware
+    {
+        [Key]
+        public Guid IdLog { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        [MaxLength(300)]
+        public string Mensaje { get; set; }
+        [MaxLength(100)]
+        public string Modulo { get; set; }
+    }
+}
