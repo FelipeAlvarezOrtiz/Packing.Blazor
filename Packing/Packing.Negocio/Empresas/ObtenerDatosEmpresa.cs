2aa98215b8eac34d592d147414e8719a31e09064
Felipe Alvarez Ortiz <me@felipealvarez.dev>
2022-01-10 21:32:18 -0300

    - Correción edición de empresa

---
+++ Packing/Packing.Negocio/Empresas/ObtenerDatosEmpresa.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Packing.Core.Empresas;
+using Packing.Persistencia;
+using Packing.Shared.EmpresaDto;
+
+namespace Packing.Negocio.Empresas
+{
+    public class ObtenerDatosEmpresa : IRequestHandler<ObtenerEmpresaQuery,Empresa>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ObtenerDatosEmpresa(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Empresa> Handle(ObtenerEmpresaQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Empresas.Where(x => x.IdEmpresa == request.IdEmpresa)
+                .Include(x => x.ProductosVisibles).ThenInclude(x => x.Producto)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
